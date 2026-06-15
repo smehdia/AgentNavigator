53a0357212599a664f21ae337a5b93a9f0f3c875
Mehdi <mehdi.ayyoubzadeh@huawei.com>
2026-06-15 16:35:43 -0400

    minor update

--- inference/run_gui.sh
+++ inference/run_gui.sh
@@ -3,6 +3,8 @@
 set -e
 cd "$(dirname "$0")"
 
+# Driver temp screenshots (e.g. __agentnav_*.jpeg) are written in this directory.
+
 unset HTTP_PROXY HTTPS_PROXY http_proxy https_proxy \
       ALL_PROXY all_proxy SOCKS_PROXY SOCKS5_PROXY \
       socks_proxy socks5_proxy FTP_PROXY ftp_proxy 2>/dev/null || true
